7887ae29a6505f36a759dacf364d7cdafcc1e800
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-03-29 22:17:55 -0400

    Add more CLI options to email-update.py
    Use the CLI options in update.sh.

--- email-update.py
+++ email-update.py
@@ -3,15 +3,20 @@
 """email-update.py is a tool for sending an updated code.gov JSON file
 
 Usage:
-  email-update.py --from=EMAIL --to=EMAIL [--reply=EMAIL] [--log-level=LEVEL]
+  email-update.py --from=EMAIL --to=EMAIL [--cc=EMAIL] [--reply=EMAIL]  --json=FILENAME --subject=SUBJECT --text=FILENAME --html=FILENAME [--log-level=LEVEL]
   email-update.py (-h | --help)
 
 Options:
-  -h --help            Show this message.
-  -f --from=EMAIL      The email address from which the updated JSON file should be sent.
-  -t --to=EMAIL        The email address where the updated JSON file should be sent.
-  -r --reply=EMAIL     The email address to use as the reply-to address when sending the updated JSON file.
-  -l --log-level=LEVEL If specified, then the log level will be set to the specified value.  Valid values are "debug", "info", "warn", and "error".
+  -h --help         Show this message.
+  --from=EMAIL      The email address from which the updated JSON file should be sent.
+  --to=EMAIL        A comma-separated list email address where the updated JSON file should be sent.
+  --cc=EMAIL        A comma-separated list email address where the updated JSON file should also be sent.
+  --reply=EMAIL     The email address to use as the reply-to address when sending the updated JSON file.
+  --json=FILENAME   The name of the updated JSON file.
+  --subject=SUBJECT The subject to use when sending the updated JSON file.
+  --text=FILENAME   The name of a file containing the plain text that is to be used as the body of the email when sending the updated JSON file.
+  --html=FILENAME   The name of a file containing the HTML text that is to be used as the body of the email when sending the updated JSON file.
+  --log-level=LEVEL If specified, then the log level will be set to the specified value.  Valid values are "debug", "info", "warn", and "error".
 
 """
 
@@ -23,21 +28,6 @@ import os
 
 import boto3
 import docopt
-
-
-class UnableToSendError(Exception):
-    """Raised when an error is encountered when attempting to send an
-email message
-
-    Attributes
-    ----------
-    response : dict
-        The response returned by boto3.
-
-    """
-
-    def __init__(self, response):
-        self.response = response
 
 
 def main():
@@ -54,51 +44,67 @@ def main():
         )
     except ValueError:
         logging.critical(
-            f'"{log_level}" is not a valid logging level.  '
-            f"Possible values are debug, info, warn, and error."
+            f'"{log_level}" is not a valid logging level.  Possible values are debug, info, warn, and error.'
         )
         return 1
 
     # Handle some command line arguments
     from_email = args["--from"]
     to_email = args["--to"]
+    cc_email = args["--cc"]
     reply_email = args["--reply"]
+    json_filename = args["--json"]
+    subject = args["--subject"]
+    text_filename = args["--text"]
+    html_filename = args["--html"]
 
+    # Build up the MIME message to be sent
     msg = MIMEMultipart("mixed")
     msg["From"] = from_email
+    logging.debug(f"Message will be sent from: {from_email}")
     msg["To"] = to_email
-    # msg['CC'] = 'cameron.dixon@trio.dhs.gov'
+    logging.debug(f"Message will be sent to: {to_email}")
+    if cc_email is not None:
+        msg["CC"] = cc_email
+        logging.debug(f"Message will be sent as CC to: {cc_email}")
     if reply_email is not None:
         msg["Reply-To"] = reply_email
+        logging.debug(f"Replies will be sent to: {reply_email}")
+    msg["Subject"] = subject
+    logging.debug(f"Message subject is: {subject}")
 
-    msg["Subject"] = "Hello"
+    # Construct and attach the text body
+    body = MIMEMultipart("alternative")
+    with open(text_filename, "r") as text:
+        t = text.read()
+        body.attach(MIMEText(t, "plain"))
+        logging.debug(f"Message plain-text body is: {t}")
+    with open(html_filename, "r") as html:
+        h = html.read()
+        html_part = MIMEText(h, "html")
+        # See https://en.wikipedia.org/wiki/MIME#Content-Disposition
+        html_part.add_header("Content-Disposition", "inline")
+        body.attach(html_part)
+        logging.debug(f"Message HTML body is: {h}")
+    msg.attach(body)
 
-    text_body = MIMEMultipart("alternative")
-    text_body.attach(MIMEText("Hello sir!", "plain"))
-    html_part = MIMEText("<h1>Hello, sir!</h1>", "html")
-    # See https://en.wikipedia.org/wiki/MIME#Content-Disposition
-    html_part.add_header("Content-Disposition", "inline")
-    text_body.attach(html_part)
-    msg.attach(text_body)
-
-    json_filename = "code.json"
+    # Attach JSON file
     with open(json_filename, "r") as attachment:
         json_part = MIMEApplication(attachment.read(), "json")
-    # See https://en.wikipedia.org/wiki/MIME#Content-Disposition
-    _, filename = os.path.split(json_filename)
-    json_part.add_header("Content-Disposition", "attachment", filename=filename)
+        # See https://en.wikipedia.org/wiki/MIME#Content-Disposition
+        _, filename = os.path.split(json_filename)
+        json_part.add_header("Content-Disposition", "attachment", filename=filename)
+        logging.debug(f"Message will include file {json_filename} as attachment")
     msg.attach(json_part)
 
+    # Send the email
     ses_client = boto3.client("ses")
     response = ses_client.send_raw_email(RawMessage={"Data": msg.as_string()})
-
     # Check for errors
     status_code = response["ResponseMetadata"]["HTTPStatusCode"]
     if status_code != 200:
-        logging.error(
-            "Unable to send message.  " "Response from boto3 is: {}".format(response)
-        )
-        raise UnableToSendError(response)
+        logging.error(f"Unable to send message.  Response from boto3 is: {response}")
+        return 2
 
     # Stop logging and clean up
     logging.shutdown()
--- update.sh
+++ update.sh
@@ -5,4 +5,13 @@ set -o errexit
 set -o pipefail
 
 scraper --config /run/secrets/scraper_config.json
-./email-update.py --from=reports@cyber.dhs.gov --to=jeremy.frasier@trio.dhs.gov
+./email-update.py \
+    --from=code@cyber.dhs.gov \
+    --to=jeremy.frasier@trio.dhs.gov \
+    --reply=ncats-dev@beta.dhs.gov \
+    --json=code.json \
+    --subject="Latest code.gov JSON file" \
+    --text=body.txt --html=body.html \
+    --log-level=debug
+
+#    --cc=ncats-dev@beta.dhs.gov \
