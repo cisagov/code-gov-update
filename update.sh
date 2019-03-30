99a0cdcc02f1a1c1445d9680eb8ff59ae5a66d7f
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-03-30 11:31:38 -0400

    Specify CLI arguments in docker-compose file
    This is better than hard-coding them in the update.sh script.

--- update.sh
+++ update.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 
+# Note that all command line arguments are passed directly to
+# email-update.py
+
 set -o nounset
 set -o errexit
 set -o pipefail
 
 scraper --config /run/secrets/scraper_config.json
-./email-update.py \
-    --from=code@cyber.dhs.gov \
-    --to=jeremy.frasier@trio.dhs.gov \
-    --reply=ncats-dev@beta.dhs.gov \
-    --json=code.json \
-    --subject="Latest code.gov JSON file" \
-    --text=body.txt --html=body.html \
-    --log-level=debug
-
-#    --cc=ncats-dev@beta.dhs.gov \
+./email-update.py "$@"
